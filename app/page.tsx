d27f24de146b413a3408326462db6d5b084bf989
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-04 03:11:53 +0330

    fixed width content in home page.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
-      <div>
+      <div className="w-full max-w-7xl">
         <h1>Hell Testing</h1>
       </div>
     </main>
