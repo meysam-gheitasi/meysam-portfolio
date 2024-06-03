b777540c63b00c2c7273bf85e713a54f2bd85d3d
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-03 04:15:59 +0330

    import costum color in tailwind config.

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative bg-black">Hello</main>
+    <main className="relative bg-black-100">Hello</main>
   )
 };
