61944e6a8a4ec316824f25b1ae9aa0ad2eb76764
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-05 02:18:57 +0330

    use Spotlight component in package and created new  styles.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,13 @@
+import { Spotlight } from "./ui/Spotlight"
 
 
 const Hero = () => {
   return (
-    <div className="">Hero</div>
+    <div className="bt-36 pb-20">
+        <Spotlight fill="white" className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" />
+        <Spotlight fill="purple" className="top-10 left-full h-[80vh] w-[50vw]" />
+        <Spotlight fill="blue" className="top-28 left-80 md:-left-32 md:-top-20 h-[80vh] w-[50vw]" />
+    </div>
   )
 }
 
