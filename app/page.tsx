f30af8449f2c5e72aede3c16c2c37c708403d5a8
Meysam Gheitasi <121888134+meysam-gheitasi@users.noreply.github.com>
2024-06-04 15:31:26 +0330

    created folder component with component Hero in import this in home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Hero from "@/component/Hero";
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex flex-col justify-center items-center overflow-hidden mx-auto px-5 sm:px-10">
       <div className="w-full max-w-7xl">
-        <h1>Hell Testing</h1>
+        <Hero />
       </div>
     </main>
   )
